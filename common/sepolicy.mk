14bca5e3d7f9f4130b889702239793e535a20a58
Paul Keith <javelinanddart@gmail.com>
2018-02-24 21:39:42 +0100

    slim_sepolicy: Add sf_lcd_density_prop type and labelled props
    * Fixes sepolicy for recovery for devices that don't
      include device/qcom/sepolicy (like Nexus/Pixel)
    
    Change-Id: Iae1e15b524514ab30634b5e896603c43616faebc

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,7 +1,10 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from Slim
 #
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/slim/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/slim/sepolicy/common/private
