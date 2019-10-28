xperiaos-temp/device_gzosp_sepolicy

sepolicy qcom: correct path
Change-Id: Id4f0e1f2cf693ea83f5b982251d44fc46673f074
Signed-off-by: leodiram <sriramscience7@gmail.com>

qcom/sepolicy.mk
@@ -4,7 +4,7 @@
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/gzosp/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
     device/gzosp/sepolicy/qcom/common \

SlimRoms14/device_slim_sepolicy

qcom/sepolicy.mk
@@ -4,7 +4,7 @@
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/slim/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
     device/slim/sepolicy/qcom/common \