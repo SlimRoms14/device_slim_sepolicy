Cosmic-OS/device_cosmic_sepolicy

sepolicy: actually load this stuff
Change-Id: I21f9c101931921abf846eaa82df2900a271597ab

common/sepolicy.mk
@@ -4,4 +4,5 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/gzosp/sepolicy/common
+    device/gzosp/sepolicy/common/vendor \
+    device/gzosp/sepolicy/common/private

SlimRoms14/device_slim_sepolicy

common/sepolicy.mk
@@ -4,4 +4,5 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/gzosp/sepolicy/common
+    device/slim/sepolicy/common/vendor \
+    device/slim/sepolicy/common/private