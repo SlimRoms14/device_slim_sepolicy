4893808b1a06cd8a40ad0304c495e740410f9e98
Michael Bestas <mkbestas@lineageos.org>
2021-10-07 22:16:53 +0300

    common: Drop custom filesystem rules
    * Support for more filesystems for public storage has
      not worked properly since 18.1.
    
    Change-Id: I604fcac815d392949fcb7f97c3d0483dc3d38005

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,9 +3,9 @@
 # inherit from Slim
 #
 
-ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-ifeq ($(TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR),true)
-BOARD_SEPOLICY_M4DEFS += board_excludes_fuseblk_sepolicy=true
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
 endif
 endif
 
